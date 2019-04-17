ecf923ed4fdb27c8e1709d3218e45d4684b0277f
Pavlo Volokonskyi <Pavlo_Volokonskyi@epam.com>
2019-04-17 16:33:27 +0300

    update

---
+++ resources.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "environment-example-two" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags {
+        Name = "terraform-aws-vpc-example-two"
+    }
+}
+
+resource "aws_subnet" "subnet01" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet02" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2b"
+}
